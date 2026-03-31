c023101ceaac7837b6224af3165ac3d5b1722450
Nada Amin <namin@alum.mit.edu>
2026-03-31 14:37:29 -0400

    minor

--- eg5.rs
+++ eg5.rs
@@ -5,10 +5,10 @@ fn main() {
         let b = &v; // immutable reference to v
         
         // ownership of v is now shared between a and b
-        // v.push(22) // ERROR
+        // v.push(22); // ERROR
         println!("The answer is {}", a[0] + b[2]);
         // ownership can be back with v
-        // v.push(22) // OK
+        // v.push(22); // OK
     }
     // ownership is now back with v
     v.push(24);
